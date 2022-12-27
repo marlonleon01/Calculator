71d93b2361698678f49fb3e962f3ed2c90cc4c7c
Marlon Leon <ghostveene@gmail.com>
2022-12-27 12:24:06 -0500

    all btn variables working, retrieve numbers field

---
+++ index.js
@@ -0,0 +1,6 @@
+//Variables for numbers && operators
+
+let numberBtns = document.querySelectorAll("[data-numbers]");
+let operationBtns = document.querySelectorAll("[data-operation]");
+let allClearBtn = document.querySelector(".all-clear-btn");
+let deleteBtn = document.querySelector(".delete-btn");
