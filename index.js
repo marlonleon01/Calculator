1c1874768eece587a0ff38804f8c6009a398b3ba
Marlon Leon <ghostveene@gmail.com>
2022-12-27 12:28:23 -0500

    all variables working

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ let numberBtns = document.querySelectorAll("[data-numbers]");
 let operationBtns = document.querySelectorAll("[data-operation]");
 let allClearBtn = document.querySelector(".all-clear-btn");
 let deleteBtn = document.querySelector(".delete-btn");
+let previousOperandTextField = document.querySelector(".previous-operand");
+let currentOperandTextField = document.querySelector(".current-operand");
+
